c4ccea2aed1d7ec680d3ceedad865f8d5a25c818
Ismail Jamiu Babatunde <phantmoney2011@gmail.com>
2022-01-19 09:02:36 +0100

    E-commerce setup with Navbar and Slider

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React, { useState } from 'react'
+import Home from './pages/Home'
+
+function App() {
+  return (
+    <div>
+      <Home />
+    </div>
+  )
+}
+
+export default App
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,88 @@
+import { Search, ShoppingCartOutlined } from '@material-ui/icons'
+import { Badge } from '@material-ui/core'
+import React from 'react'
+import styled from 'styled-components'
+
+const Container = styled.div`
+  height: 60px;
+  /* background-color: black; */
+`
+const Wrapper = styled.div`
+  padding: 10px 20px;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+`
+const Left = styled.div`
+  flex: 1;
+  display: flex;
+  align-items: center;
+`
+
+const Language = styled.span`
+  font-size: 14px;
+  cursor: pointer;
+`
+const SearchContainer = styled.div`
+  border: 0.5px solid lightgray;
+  display: flex;
+  margin-left: 25px;
+  padding: 5px;
+  align-items: center;
+`
+
+const Input = styled.input`
+  border: none;
+`
+
+const Center = styled.h1`
+  flex: 1;
+  text-align: center;
+`
+const Logo = styled.h1`
+  font-weight: bold;
+`
+
+const Right = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: flex-end;
+  flex: 1;
+`
+const MenuItem = styled.div`
+  font-size: 14px;
+  margin-left: 25px;
+  cursor: pointer;
+`
+
+const Navbar = () => {
+  return (
+    <Container>
+      <Wrapper>
+        <Left>
+          <Language>ENG</Language>
+          <SearchContainer>
+            <Input />
+            <Search style={{ color: 'gray', fontSize: 16 }} />
+          </SearchContainer>
+        </Left>
+        <Center>
+          <Logo />
+          Phant Wears
+        </Center>
+        <Right>
+          <MenuItem>REGISTER</MenuItem>
+          <MenuItem>SIGN IN </MenuItem>
+          <MenuItem>
+            <Badge badgeContent={10} color='primary'>
+              <ShoppingCartOutlined />
+            </Badge>
+          </MenuItem>
+        </Right>
+        <MenuItem>REGISTER</MenuItem>
+      </Wrapper>
+    </Container>
+  )
+}
+
+export default Navbar
