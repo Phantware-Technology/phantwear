e054bbd2d986785ffd6f62747ba06a1d55495500
Ismail Jamiu Babatunde <phantmoney2011@gmail.com>
2022-01-20 16:39:57 +0100

    Cart Implemented

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import React, { useState } from 'react'
-import Login from './pages/Login'
+import Cart from './pages/Cart'
+// import Login from './pages/Login'
 // import Home from './pages/Home'
 // import Register from './pages/Register'
 
 function App() {
   return (
     <div>
-      <Login />
+      <Cart />
     </div>
   )
 }
