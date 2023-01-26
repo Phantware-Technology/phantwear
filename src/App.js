78c0afd54c5122bacdedc565d5eaea89b2db87f2
phantware <phantmoney2011@gmail.com>
2023-01-26 22:00:50 +0100

    routing defined

--- src/App.js
+++ src/App.js
@@ -1,9 +1,24 @@
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import Home from './pages/Home'
+import ProductList from './pages/ProductList'
+import Product from './pages/Product'
+import Cart from './pages/Cart'
+import Login from './pages/Login'
+import Register from './pages/Register'
 
 function App() {
   return (
     <div>
-      <Home />
+      <BrowserRouter>
+        <Routes>
+          <Route path='/' element={<Home />} />
+          <Route path='/products/:category' element={<ProductList />} />
+          <Route path='/product/:id' element={<Product />} />
+          <Route path='/cart' element={<Cart />} />
+          <Route path='/login' element={<Login />} />
+          <Route path='/register' element={<Register />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   )
 }
