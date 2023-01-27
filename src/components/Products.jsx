aad03c29c625bfc694d1d9c71e11d677d43365f8
phantware <phantmoney2011@gmail.com>
2023-01-27 16:47:08 +0100

    axiosInstance file modified

--- src/components/Products.jsx
+++ src/components/Products.jsx
@@ -1,7 +1,8 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import styled from 'styled-components'
 import { popularProducts } from '../data'
 import Product from './Product'
+import axiosInstance from '../utils/axiosInstance'
 
 const Container = styled.div`
   padding: 20px;
@@ -13,6 +14,17 @@ const Container = styled.div`
 const Products = ({ cat, sort, filters }) => {
   const [products, setProducts] = useState([])
   const [filteredProducts, setFilteredProducts] = useState([])
+
+  useEffect(() => {
+    const getProducts = async () => {
+      try {
+        const res = await axiosInstance.get('/products')
+        console.log('res', res)
+        // const data = await res.json()
+      } catch (error) {}
+    }
+    getProducts()
+  }, [cat])
   return (
     <Container>
       {popularProducts.map((item) => {
