290fbd1f6af5be7ab0936ce2f0cd230648003e4d
phantware <phantmoney2011@gmail.com>
2023-01-28 18:16:20 +0100

    products and productList page modified

--- src/components/Products.jsx
+++ src/components/Products.jsx
@@ -1,6 +1,5 @@
 import { useEffect, useState } from 'react'
 import styled from 'styled-components'
-import { popularProducts } from '../data'
 import Product from './Product'
 import axiosInstance from '../utils/axiosInstance'
 
@@ -11,7 +10,7 @@ const Container = styled.div`
   justify-content: space-between;
 `
 
-const Products = ({ cat, sort, filters }) => {
+const Products = ({ cat, filters, sort }) => {
   const [products, setProducts] = useState([])
   const [filteredProducts, setFilteredProducts] = useState([])
 
@@ -19,19 +18,43 @@ const Products = ({ cat, sort, filters }) => {
     const getProducts = async () => {
       try {
         const res = await axiosInstance.get(
-          cat ? `/products?category=${cat}` : '/products'
+          cat ? `products?category=${cat}` : 'products'
         )
-        console.log('res', res)
-        // const data = await res.json()
-      } catch (error) {}
+        setProducts(res.data)
+      } catch (err) {}
     }
     getProducts()
   }, [cat])
+  useEffect(() => {
+    cat &&
+      setFilteredProducts(
+        products.filter((item) =>
+          Object.entries(filters).every(([key, value]) =>
+            item[key].includes(value)
+          )
+        )
+      )
+  }, [products, cat, filters])
+
+  useEffect(() => {
+    if (sort === 'newest') {
+      setFilteredProducts((prev) =>
+        [...prev].sort((a, b) => a.createdAt - b.createdAt)
+      )
+    } else if (sort === 'asc') {
+      setFilteredProducts((prev) => [...prev].sort((a, b) => a.price - b.price))
+    } else {
+      setFilteredProducts((prev) => [...prev].sort((a, b) => b.price - a.price))
+    }
+  }, [sort])
+
   return (
     <Container>
-      {popularProducts.map((item) => {
-        return <Product item={item} key={item.id} />
-      })}
+      {cat
+        ? filteredProducts.map((item) => <Product item={item} key={item.id} />)
+        : products
+            .slice(0, 8)
+            .map((item) => <Product item={item} key={item.id} />)}
     </Container>
   )
 }
--- src/pages/ProductList.jsx
+++ src/pages/ProductList.jsx
@@ -1,25 +1,29 @@
+import styled from 'styled-components'
 import Navbar from '../components/Navbar'
 import Announcement from '../components/Announcement'
 import Products from '../components/Products'
 import Newsletter from '../components/Newsletter'
 import Footer from '../components/Footer'
 import { mobile } from '../responsive'
-import styled from 'styled-components'
-import { useLocation } from 'react-router-dom'
+import { useLocation } from 'react-router'
 import { useState } from 'react'
 
 const Container = styled.div``
+
 const Title = styled.h1`
   margin: 20px;
 `
+
 const FilterContainer = styled.div`
   display: flex;
   justify-content: space-between;
 `
+
 const Filter = styled.div`
   margin: 20px;
   ${mobile({ width: '0px 20px', display: 'flex', flexDirection: 'column' })}
 `
+
 const FilterText = styled.span`
   font-size: 20px;
   font-weight: 600;
@@ -52,18 +56,18 @@ const ProductList = () => {
     <Container>
       <Navbar />
       <Announcement />
-      <Title>Dresses</Title>
+      <Title>{cat}</Title>
       <FilterContainer>
         <Filter>
-          <FilterText>Filter Products</FilterText>
+          <FilterText>Filter Products:</FilterText>
           <Select name='color' onChange={handleFilters}>
             <Option disabled>Color</Option>
-            <Option>White</Option>
-            <Option>Black</Option>
-            <Option>Red</Option>
-            <Option>Blue</Option>
-            <Option>Yellow</Option>
-            <Option>Green</Option>
+            <Option>white</Option>
+            <Option>black</Option>
+            <Option>red</Option>
+            <Option>blue</Option>
+            <Option>yellow</Option>
+            <Option>green</Option>
           </Select>
           <Select name='size' onChange={handleFilters}>
             <Option disabled>Size</Option>
@@ -75,7 +79,7 @@ const ProductList = () => {
           </Select>
         </Filter>
         <Filter>
-          <FilterText>Sort Products</FilterText>
+          <FilterText>Sort Products:</FilterText>
           <Select onChange={(e) => setSort(e.target.value)}>
             <Option value='newest'>Newest</Option>
             <Option value='asc'>Price (asc)</Option>
@@ -83,7 +87,7 @@ const ProductList = () => {
           </Select>
         </Filter>
       </FilterContainer>
-      <Products cat={cat} sort={sort} filters={filters} />
+      <Products cat={cat} filters={filters} sort={sort} />
       <Newsletter />
       <Footer />
     </Container>
